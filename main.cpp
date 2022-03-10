e3d02ae113539e11680608774dffcfd211f96d15
Phil Castaneda <pjcast@users.noreply.github.com>
2022-03-10 15:12:29 -0800

    Playing around with udev

--- main.cpp
+++ main.cpp
@@ -9,8 +9,100 @@
 #include <map>
 #include "OIS2InputManager.h"
 
+//https://github.com/gavv/snippets/blob/master/udev/udev_monitor_usb.c
+#include <libudev.h>
+#include <stdio.h>
+
 using namespace std;
 using namespace OIS2;
+
+static struct udev_device* get_child(struct udev* udev, struct udev_device* parent, const char* subsystem)
+{
+	struct udev_device* child = NULL;
+	struct udev_enumerate *enumerate = udev_enumerate_new(udev);
+
+	udev_enumerate_add_match_parent(enumerate, parent);
+	udev_enumerate_add_match_subsystem(enumerate, subsystem);
+	udev_enumerate_scan_devices(enumerate);
+
+	struct udev_list_entry *devices = udev_enumerate_get_list_entry(enumerate);
+	struct udev_list_entry *entry;
+
+	udev_list_entry_foreach(entry, devices) {
+		const char *path = udev_list_entry_get_name(entry);
+		child = udev_device_new_from_syspath(udev, path);
+		break;
+	}
+
+	udev_enumerate_unref(enumerate);
+	return child;
+}
+
+void enumerate_subsystems(struct udev* udev, std::vector<std::string> &subsystems)
+{
+	auto enumerate = udev_enumerate_new(udev);
+
+	udev_enumerate_scan_subsystems(enumerate);
+
+	struct udev_list_entry *devices = udev_enumerate_get_list_entry(enumerate);
+	struct udev_list_entry *entry;
+
+	udev_list_entry_foreach(entry, devices) 
+	{
+		const char* path = udev_list_entry_get_name(entry);
+		struct udev_device* dev = udev_device_new_from_syspath(udev, path);
+
+		printf("Subsystem: path:%s\n", path);
+
+		udev_device_unref(dev);
+	}
+
+
+	udev_enumerate_unref(enumerate);
+}
+
+void enumerate_usb_mass_storage(struct udev* udev)
+{
+	struct udev_enumerate* enumerate = udev_enumerate_new(udev);
+
+	udev_enumerate_add_match_subsystem(enumerate, "usb");
+	udev_enumerate_add_match_property(enumerate, "DEVTYPE", "input");
+	udev_enumerate_scan_devices(enumerate);
+
+	struct udev_list_entry *devices = udev_enumerate_get_list_entry(enumerate);
+	struct udev_list_entry *entry;
+
+	udev_list_entry_foreach(entry, devices) {
+		const char* path = udev_list_entry_get_name(entry);
+		struct udev_device* scsi = udev_device_new_from_syspath(udev, path);
+		printf("Hello\n");
+
+		//struct udev_device* block = get_child(udev, scsi, "block");
+		//struct udev_device* scsi_disk = get_child(udev, scsi, "scsi_disk");
+
+		//struct udev_device* usb = udev_device_get_parent_with_subsystem_devtype(scsi, "usb", "usb_device");
+
+		// if (block && scsi_disk && usb) {
+		//     printf("block = %s, usb = %s:%s, scsi = %s\n",
+		//            udev_device_get_devnode(block),
+		//            udev_device_get_sysattr_value(usb, "idVendor"),
+		//            udev_device_get_sysattr_value(usb, "idProduct"),
+		//            udev_device_get_sysattr_value(scsi, "vendor"));
+		// }
+
+		// if (block) {
+		//     udev_device_unref(block);
+		// }
+
+		// if (scsi_disk) {
+		// 	udev_device_unref(scsi_disk);
+		// }
+
+		// udev_device_unref(scsi);
+	}
+
+	udev_enumerate_unref(enumerate);
+}
 
 void TakesReference(const InputManager& happy)
 {
@@ -72,28 +164,37 @@ void DeviceDisconnectedCallback(const DeviceDescriptor& device)
 
 int main()
 {
-	auto inputManager = std::make_unique<InputManager>(ILogger::UseStdLogger());
+	struct udev* udev = udev_new();
 
-	auto someLambda = [inputManager = move(inputManager)]() 
-	{
-		std::cout << "In Lambda" << std::endl;
-		TakesReference(*inputManager);
-		TakesConstPtr(inputManager.get());
+	std::vector<std::string> subsystems;
+	enumerate_subsystems(udev, subsystems);
 
-		inputManager->SetDeviceConnectedCallback(std::bind(DeviceConnectedCallback, placeholders::_1));
-		inputManager->SetDeviceDisconnectedCallback(std::bind(DeviceDisconnectedCallback, placeholders::_1));
+	udev_unref(udev);
 
-		cout << "Current Devices: \n";
-		auto devices = inputManager->GetDiscoveredDevices();
-		for(auto device : devices)
-		{
-			cout << device;
-		}
+	// /sys/class/input
 
-		cout << "Press any key: \n";
-		getchar();
-		cout << "Shutting down... (currently, takes ~10 secs to end)\n";
-	};
+	// auto inputManager = std::make_unique<InputManager>(ILogger::UseStdLogger());
 
-	someLambda();
+	// auto someLambda = [inputManager = move(inputManager)]() 
+	// {
+	// 	std::cout << "In Lambda" << std::endl;
+	// 	TakesReference(*inputManager);
+	// 	TakesConstPtr(inputManager.get());
+
+	// 	inputManager->SetDeviceConnectedCallback(std::bind(DeviceConnectedCallback, placeholders::_1));
+	// 	inputManager->SetDeviceDisconnectedCallback(std::bind(DeviceDisconnectedCallback, placeholders::_1));
+
+	// 	cout << "Current Devices: \n";
+	// 	auto devices = inputManager->GetDiscoveredDevices();
+	// 	for(auto device : devices)
+	// 	{
+	// 		cout << device;
+	// 	}
+
+	// 	cout << "Press any key: \n";
+	// 	getchar();
+	// 	cout << "Shutting down... (currently, takes ~10 secs to end)\n";
+	// };
+
+	// someLambda();
 }
